9e9d264b2d2baad7af2c281c6ce03a442f49fd0e
Virxhin Bytyqi <xhini95@gmail.com>
2026-04-28 15:39:46 +0200

    feat: redesign projects section with Figma-style cards and add client projects
    Rebuilds project section as a full-bleed slider with large accent number, tech badges, GitHub/Live buttons, and image frame. Adds Clearifix and Permacare24 as real client projects (05).

--- src/app/features/portfolio/projects/projects.ts
+++ src/app/features/portfolio/projects/projects.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
-interface Project {
-  key: string;
-  titleKey: string;
+export interface Project {
+  number: string;
+  title: string;
   descKey: string;
-  techsKey: string;
+  techs: string[];
   github: string;
   live: string;
   image: string;
@@ -17,24 +17,69 @@ interface Project {
   styleUrl: './projects.scss',
 })
 export class Projects {
+  currentIndex = 0;
+
   projects: Project[] = [
     {
-      key: 'join',
-      titleKey: 'Join',
+      number: '01',
+      title: 'Join',
       descKey: 'PROJECTS.JOIN_DESC',
-      techsKey: 'PROJECTS.JOIN_TECHS',
+      techs: ['HTML', 'CSS', 'JavaScript', 'Firebase'],
       github: 'https://github.com/babalushi04/join',
       live: '',
-      image: 'assets/img/projects/join-preview.png',
+      image: 'assets/img/projects/join-preview.svg',
     },
     {
-      key: 'elpollo',
-      titleKey: 'El Pollo Loco',
+      number: '02',
+      title: 'El Pollo Loco',
       descKey: 'PROJECTS.ELPOLLO_DESC',
-      techsKey: 'PROJECTS.ELPOLLO_TECHS',
-      github: 'https://github.com/babalushi04/el-pollo-loco',
+      techs: ['HTML', 'CSS', 'JavaScript'],
+      github: 'https://github.com/babalushi04/EL-Pollo-Loco',
       live: '',
-      image: 'assets/img/projects/elpollo-preview.png',
+      image: 'assets/img/projects/elpollo-preview.svg',
+    },
+    {
+      number: '03',
+      title: 'Kochwelt',
+      descKey: 'PROJECTS.KOCHWELT_DESC',
+      techs: ['HTML', 'CSS', 'JavaScript', 'Firebase'],
+      github: 'https://github.com/babalushi04/kochwelt',
+      live: '',
+      image: 'assets/img/projects/kochwelt-preview.svg',
+    },
+    {
+      number: '04',
+      title: 'Clearifix',
+      descKey: 'PROJECTS.CLEARIFIX_DESC',
+      techs: ['HTML', 'CSS', 'JavaScript'],
+      github: '',
+      live: 'https://www.clearifix.com',
+      image: 'assets/img/projects/clearifix-preview.svg',
+    },
+    {
+      number: '05',
+      title: 'Permacare24',
+      descKey: 'PROJECTS.PERMACARE_DESC',
+      techs: ['HTML', 'CSS', 'JavaScript'],
+      github: '',
+      live: 'https://www.permacare24.com',
+      image: 'assets/img/projects/permacare-preview.svg',
     },
   ];
+
+  get current(): Project {
+    return this.projects[this.currentIndex];
+  }
+
+  next(): void {
+    this.currentIndex = (this.currentIndex + 1) % this.projects.length;
+  }
+
+  prev(): void {
+    this.currentIndex = (this.currentIndex - 1 + this.projects.length) % this.projects.length;
+  }
+
+  goTo(index: number): void {
+    this.currentIndex = index;
+  }
 }
